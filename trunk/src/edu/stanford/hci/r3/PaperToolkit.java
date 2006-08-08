f53695fdd851f74553f96a66acc3c5391635d687
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-08-08 02:03:23 +0000

    Default Pattern

---
+++ trunk/src/edu/stanford/hci/r3/PaperToolkit.java
@@ -0,0 +1,26 @@
+package edu.stanford.hci.r3;
+
+/**
+ * <p>
+ * This software is distributed under the <a href="http://hci.stanford.edu/research/copyright.txt">
+ * BSD License</a>.
+ * </p>
+ * 
+ * @author <a href="http://graphics.stanford.edu/~ronyeh">Ron B Yeh</a> (ronyeh(AT)cs.stanford.edu)
+ */
+public class PaperToolkit {
+
+	/**
+	 * 
+	 */
+	public PaperToolkit() {
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+
+}
