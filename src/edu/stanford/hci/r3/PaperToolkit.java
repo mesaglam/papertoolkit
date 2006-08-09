2ab96bd7824360859613d77b7f568a26583f6ba2
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-08-09 03:43:55 +0000

    Added Support JARS. Added Documentation. Added Example Code.

--- src/edu/stanford/hci/r3/PaperToolkit.java
+++ src/edu/stanford/hci/r3/PaperToolkit.java
@@ -1,5 +1,7 @@
 package edu.stanford.hci.r3;
 
+import edu.stanford.hci.r3.util.WindowUtils;
+
 /**
  * <p>
  * This software is distributed under the <a href="http://hci.stanford.edu/research/copyright.txt">
@@ -10,11 +12,20 @@ package edu.stanford.hci.r3;
  */
 public class PaperToolkit {
 
+	
+	
 	/**
 	 * 
 	 */
 	public PaperToolkit() {
+		initializeSwing();
+	}
 
+	/**
+	 * Sets up parameters for any Java Swing UI we need.
+	 */
+	private void initializeSwing() {
+		WindowUtils.setNativeLookAndFeel();
 	}
 
 	/**
---
+++ src/edu/stanford/hci/r3/printing/java2d/Java2DPrinter.java
@@ -0,0 +1,5 @@
+package edu.stanford.hci.r3.printing.java2d;
+
+public class Java2DPrinter {
+
+}
