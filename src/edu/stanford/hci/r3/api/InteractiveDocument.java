41726f58b4c8df68d8f94db446118b8944342065
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-08-16 21:45:29 +0000

    We can print PDFs now... How can we make it a nicer API?

---
+++ src/edu/stanford/hci/r3/api/InteractiveDocument.java
@@ -0,0 +1,15 @@
+package edu.stanford.hci.r3.api;
+
+/**
+ * <p>
+ * This software is distributed under the <a
+ * href="http://hci.stanford.edu/research/copyright.txt"> BSD License</a>.
+ * </p>
+ * 
+ * @author <a href="http://graphics.stanford.edu/~ronyeh">Ron B Yeh</a> (ronyeh(AT)cs.stanford.edu)
+ * 
+ * A collection of pages/sheets of interactive paper. A document contains multiple InteractiveSheets.
+ */
+public class InteractiveDocument {
+
+}
--- src/edu/stanford/hci/r3/pattern/output/PDFPatternGenerator.java
+++ src/edu/stanford/hci/r3/pattern/output/PDFPatternGenerator.java
@@ -8,9 +8,8 @@ import java.io.IOException;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
-import com.lowagie.text.Font;
+import com.lowagie.text.Image;
 import com.lowagie.text.PageSize;
-import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.BaseFont;
 import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfWriter;
@@ -29,8 +28,13 @@ import edu.stanford.hci.r3.units.PatternDots;
  * 
  * @author <a href="http://graphics.stanford.edu/~ronyeh">Ron B Yeh</a> (ronyeh(AT)cs.stanford.edu)
  * 
+ * <p>
  * This sits on top of PatternPackage and/or TiledPattern to create PDFs that can be printed. It
  * uses the iText library to create and manipulate PDFs.
+ * </p>
+ * 
+ * So far, printing dots with the bullet point • in Tahoma works decently. We will also try drawing
+ * circles, or using some sort of stamping pattern if possible.
  */
 public class PDFPatternGenerator {
 
@@ -44,16 +48,18 @@ public class PDFPatternGenerator {
 
 	private static final int Y_FONT_OFFSET = 9;
 
-	private static final float FONT_SIZE = 30;
-	
+	private static final float FONT_SIZE = 29;
 
 	public static void main(String[] args) {
 		TiledPatternGenerator generator = new TiledPatternGenerator();
 		PatternPackage pkg = generator.getCurrentPatternPackage();
 		String[] pattern = pkg.readPatternFromFile(0, new PatternDots(0), new PatternDots(0),
-				new Inches(2), new Inches(2));
+				new Inches(.5), new Inches(5));
 
 		try {
+			
+			System.out.println(PageSize.LETTER);
+			
 			Document document = new Document(PageSize.LETTER, 50, 50, 50, 50);
 			PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(
 					"testData/test.pdf"));
@@ -65,24 +71,31 @@ public class PDFPatternGenerator {
 			// access the document
 			document.open();
 
+			
 			// write direct content
 			PdfContentByte cb = writer.getDirectContent();
 
 			cb.beginText();
 			cb.setFontAndSize(bfont, 12);
-			cb.showTextAligned(PdfContentByte.ALIGN_LEFT, "Tahoma " + FONT_SIZE, 288, 288, 0);
+
+			// GRAY, etc. do not work! The printer will do halftoning, which messes things up.
+			cb.setColorFill(Color.BLACK);
+
+			cb.showTextAligned(PdfContentByte.ALIGN_LEFT, "Tahoma " + FONT_SIZE + " Padding: "
+					+ DEFAULT_PADDING, 288, 288, 0);
 			cb.endText();
-			
+
 			// convert from points (72 in an inch, to 1/100 of a millimeter)
 			cb.concatCTM(1f, 0f, 0f, -1f, 0f, PageSize.LETTER.height());
 			cb.transform(AffineTransform.getScaleInstance(72 / 2540.0, 72 / 2540.0));
 
 			cb.beginText();
 			cb.setFontAndSize(bfont, FONT_SIZE);
-			cb.setColorFill(Color.BLACK);
 
-			int gridXPosition = 0;
-			int gridYPosition = 0;
+			final int initX = 72;
+
+			int gridXPosition = initX;
+			int gridYPosition = 72;
 
 			int xJitter = 0;
 			int yJitter = 0;
@@ -91,18 +104,29 @@ public class PDFPatternGenerator {
 			for (String patternRow : pattern) {
 				int rowLength = patternRow.length();
 				for (int i = 0; i < rowLength; i++) {
+
+					// read the direction
 					currentJitterDirection = patternRow.charAt(i);
+
+					// reset the jitters (this is key!)
+					xJitter = 0;
+					yJitter = 0;
+
 					switch (currentJitterDirection) {
 					case PatternJitter.DOWN:
+						// System.out.print("d");
 						yJitter = DEFAULT_JITTER;
 						break;
 					case PatternJitter.UP:
+						// System.out.print("u");
 						yJitter = -DEFAULT_JITTER;
 						break;
 					case PatternJitter.LEFT:
+						// System.out.print("l");
 						xJitter = -DEFAULT_JITTER;
 						break;
 					case PatternJitter.RIGHT:
+						// System.out.print("r");
 						xJitter = DEFAULT_JITTER;
 						break;
 					}
@@ -115,11 +139,19 @@ public class PDFPatternGenerator {
 
 					gridXPosition += DEFAULT_PADDING;
 				}
-				gridXPosition = 0;
+				gridXPosition = initX;
 				gridYPosition += DEFAULT_PADDING;
+				// System.out.println();
 			}
 
 			cb.endText();
+
+			// add images here
+			Image dragon = Image.getInstance("testData/dragon.jpg");
+			dragon.scalePercent(50f);
+			dragon.setAbsolutePosition(0, 0);
+			System.out.println(dragon.isDeflated());
+			document.add(dragon);
 
 			document.close();
 		} catch (FileNotFoundException e) {
