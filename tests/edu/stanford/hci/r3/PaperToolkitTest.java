c5c85aa0b45aa246025b4a7f65bae3df77f445b6
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-08-25 07:37:47 +0000

    Massive Refactoring so that the resources can be accessed from the JAR file.

---
+++ tests/edu/stanford/hci/r3/PaperToolkitTest.java
@@ -0,0 +1,18 @@
+package edu.stanford.hci.r3;
+
+/**
+ * <p>
+ * This software is distributed under the <a href="http://hci.stanford.edu/research/copyright.txt">
+ * BSD License</a>.
+ * </p>
+ * 
+ * @author <a href="http://graphics.stanford.edu/~ronyeh">Ron B Yeh</a> (ronyeh(AT)cs.stanford.edu)
+ */
+public class PaperToolkitTest {
+	/**
+	 * Aug 25, 2006
+	 */
+	public static void main(String[] args) {
+		new PaperToolkit();
+	}
+}
