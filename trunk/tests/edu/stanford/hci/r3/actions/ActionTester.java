10db91f4ac1b84a8d2d6333cc89af032a3e70140
joelrbrandt <joelrbrandt@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-08-28 05:08:00 +0000

    Added an "actions" package to the testing code.  Basically, I just wanted to make sure that committing works for me.

---
+++ trunk/tests/edu/stanford/hci/r3/actions/ActionTester.java
@@ -0,0 +1,13 @@
+package edu.stanford.hci.r3.actions;
+
+public class ActionTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Foo");
+
+	}
+
+}
