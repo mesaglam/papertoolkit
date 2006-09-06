15649bc47a0611339b00dc7cb3a5fae99a5acba2
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-09-06 19:02:09 +0000

    Gotta support some simple actions.

---
+++ trunk/src/edu/stanford/hci/r3/actions/types/PlaySoundAction.java
@@ -0,0 +1,19 @@
+package edu.stanford.hci.r3.actions.types;
+
+import edu.stanford.hci.r3.actions.R3Action;
+
+/**
+ * 
+ * <p>
+ * <span class="BSDLicense"> This software is distributed under the <a
+ * href="http://hci.stanford.edu/research/copyright.txt">BSD License</a>. </span>
+ * </p>
+ * 
+ * @author <a href="http://graphics.stanford.edu/~ronyeh">Ron B Yeh</a> (ronyeh(AT)cs.stanford.edu)
+ */
+public class PlaySoundAction implements R3Action {
+
+	public void invoke() {
+
+	}
+}
