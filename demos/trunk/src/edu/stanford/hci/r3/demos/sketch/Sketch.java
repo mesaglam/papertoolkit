0f58f380c6f7b5afa0e15a4de90f498bac7d8f0b
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-09-08 06:20:14 +0000

    Working on Sketch!

--- demos/trunk/src/edu/stanford/hci/r3/demos/sketch/Sketch.java
+++ demos/trunk/src/edu/stanford/hci/r3/demos/sketch/Sketch.java
@@ -1,5 +1,10 @@
 package edu.stanford.hci.r3.demos.sketch;
 
+import java.io.File;
+
+import edu.stanford.hci.r3.paper.Region;
+import edu.stanford.hci.r3.paper.sheets.PDFSheet;
+
 /**
  * <p>
  * A Hello World application, that allows a user to sketch on a piece of paper and choose various
@@ -14,4 +19,13 @@ package edu.stanford.hci.r3.demos.sketch;
  */
 public class Sketch {
 
+	public static void main(String[] args) {
+
+		PDFSheet s = new PDFSheet(new File("data/Sketch/SketchUI.pdf"));
+		s.addRegions(new File("data/Sketch/SketchUI.regions.xml"));
+		
+		for (Region r : s.getRegions()) {
+			System.out.println(r);
+		}
+	}
 }
