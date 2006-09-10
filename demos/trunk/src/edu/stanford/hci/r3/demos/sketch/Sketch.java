71d77866f0f1ae99979dd25e978416e1f2256284
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-09-10 07:50:25 +0000

    Stupid dlls.

--- demos/trunk/src/edu/stanford/hci/r3/demos/sketch/Sketch.java
+++ demos/trunk/src/edu/stanford/hci/r3/demos/sketch/Sketch.java
@@ -3,8 +3,10 @@ package edu.stanford.hci.r3.demos.sketch;
 import java.io.File;
 
 import edu.stanford.hci.r3.Application;
+import edu.stanford.hci.r3.PaperToolkit;
 import edu.stanford.hci.r3.paper.Region;
 import edu.stanford.hci.r3.paper.sheets.PDFSheet;
+import edu.stanford.hci.r3.pen.Pen;
 import edu.stanford.hci.r3.render.sheets.PDFSheetRenderer;
 
 /**
@@ -58,29 +60,40 @@ public class Sketch {
 		PDFSheetRenderer renderer = new PDFSheetRenderer(s);
 
 		// for my laser printer... calling this once makes it look better (although it still works
-		// otherwise) You may want to play around to see how many times you want to call this method.
+		// otherwise) You may want to play around to see how many times you want to call this
+		// method.
 		renderer.useSmallerPatternDots();
-		
+
 		renderer.renderToPDF(new File("data/Sketch/SketchUI_Patterned.pdf"));
 		renderer.savePatternInformation();
 		System.out.println("Done Rendering and Saving Configuration to Disk");
-		System.exit(0);
 	}
 
 	/**
 	 * @param s
 	 */
 	private static void runApplication(PDFSheet s) {
-		// check out their pattern mappings...
-
-		// they should automatically load the .patternInfo.xml if it exists next to the PDF file
-
 		// add some event handlers to the regions...
+		System.out.println("Regions: ");
 		for (Region r : s.getRegions()) {
-			System.out.println(r);
+			System.out.println("\t" + r.getName());
 		}
-		
+		System.out.println();
+
+		Region region = s.getRegion("MainDrawingArea");
+		System.out.println(region);
+
 		Application app = new Application("Sketch!");
 		app.addSheet(s);
+
+		Pen pen = new Pen("Main Pen");
+		pen.startLiveMode(); // the pen is attached to the local machine
+		// if you are adventurous, you can add listeners DIRECTLY to the pen
+		// however, that's too low level for us, so we'll add listeners to the regions instead
+
+		app.addPen(pen);
+
+		PaperToolkit r3 = new PaperToolkit();
+		r3.startApplication(app);
 	}
 }
