b25204613ff9f9cfa4d86ac1c1288320e8e84568
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-09-26 07:27:10 +0000

    Debugging the commenting stuff some more...

--- demos/trunk/src/edu/stanford/hci/r3/demos/blog/CommentCollector.java
+++ demos/trunk/src/edu/stanford/hci/r3/demos/blog/CommentCollector.java
@@ -12,43 +12,49 @@ public class CommentCollector extends InkCollector {
 
 	private BoingBoingEntry bbentry;
 	private Thread updater;
-	
+
 	private int index = 0;
 	private int count = 0;
-	
-	private static final long ONE_MINUTE = 60L * 1000L;
-	
+
+	private static final long ONE_MINUTE = 10L * 1000L;
+
 	public CommentCollector(BoingBoingEntry bbentry, int index) {
 		this.bbentry = bbentry;
 		this.index = index;
 		updater = new Thread() {
+			
+			long lastRecorded = 0L;
+			
 			public void run() {
-			 	try {
-					sleep(CommentCollector.ONE_MINUTE);
-				} catch (InterruptedException e) { }
-			long last = getLastTimestamp();
-			long now = System.currentTimeMillis();
-			if (last > 0 && now - last > CommentCollector.ONE_MINUTE) {
-				CommentCollector.this.storeComment();
-			}
+				while (true) {
+					try {
+						sleep(CommentCollector.ONE_MINUTE);
+					} catch (InterruptedException e) { }
+					long last = getLastTimestamp();
+					long now = System.currentTimeMillis();
+					if (last > 0 && now - last > CommentCollector.ONE_MINUTE && lastRecorded < last) {
+						CommentCollector.this.storeComment();
+						lastRecorded = last;
+					}
+				}
 			}
 		};
 		updater.start();
 	}
-	
+
 	void storeComment() {
 		Ink comment_ink = getInk();
-		clear();
-		
-		File comment_image = new File("data/Blog/comments/comment" + index + "_" + count); 
+		// clear();
+
+		File comment_image = new File("data/Blog/comments/comment" + index + "_" + count + ".jpg"); 
 		
 		InkRenderer r = new InkRenderer(comment_ink);
-		r.renderToJPEG(comment_image, new Pixels(72), new Inches(8.5), new Inches(8.5));
-		
+		r.renderToJPEG(comment_image, new Pixels(1), new Inches(8.5), new Inches(8.5));
+
 		// TODO: Update RSS feed here
-		
+
 		count++;
-		
+
 	}
-	
+
 }
--- demos/trunk/src/edu/stanford/hci/r3/demos/blog/CommentTester.java
+++ demos/trunk/src/edu/stanford/hci/r3/demos/blog/CommentTester.java
@@ -1,7 +1,12 @@
 package edu.stanford.hci.r3.demos.blog;
 
+import java.io.File;
+
 import edu.stanford.hci.r3.Application;
 import edu.stanford.hci.r3.PaperToolkit;
+import edu.stanford.hci.r3.events.ContentFilterListener;
+import edu.stanford.hci.r3.events.PenEvent;
+import edu.stanford.hci.r3.events.handlers.ClickHandler;
 import edu.stanford.hci.r3.paper.Region;
 import edu.stanford.hci.r3.paper.Sheet;
 
@@ -9,16 +14,18 @@ public class CommentTester extends Application {
 
 	public CommentTester() {
 		super("Comment Tester");
-		
+				
 		Sheet s = new Sheet(8.5, 11);
 		
-		Region r = new Region("CommentTest", 0.5, 0.5, 7.5, 7.5);
-
-		r.addContentFilter(new CommentCollector(null, 0));
+		s.registerConfigurationPath(new File("."));
 		
+		Region r = new Region("CommentTest", 0.5, 0.5, 7.5, 7.5);
+		CommentCollector cc = new CommentCollector(null,0);
+		r.addContentFilter(cc);
 		s.addRegion(r);
 		
 		addSheet(s);
+		
 	}
 
 	public static void main(String[] args) {
