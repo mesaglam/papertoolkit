f2682b2b5082cf79f2a73781628f3c4743b68a16
joelrbrandt <joelrbrandt@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-09-27 23:03:17 +0000

    Added RSS generation stuff for comments

--- demos/trunk/src/edu/stanford/hci/r3/demos/blog/CommentCollector.java
+++ demos/trunk/src/edu/stanford/hci/r3/demos/blog/CommentCollector.java
@@ -1,6 +1,21 @@
 package edu.stanford.hci.r3.demos.blog;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.sun.syndication.feed.synd.SyndContent;
+import com.sun.syndication.feed.synd.SyndContentImpl;
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndEntryImpl;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.feed.synd.SyndFeedImpl;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedOutput;
 
 import edu.stanford.hci.r3.events.filters.InkCollector;
 import edu.stanford.hci.r3.pen.ink.Ink;
@@ -18,14 +33,27 @@ public class CommentCollector extends InkCollector {
 	private int count = 0;
 
 	private static final long ONE_MINUTE = 10L * 1000L;
+	private static final String COMMENT_DIR = "C:/www/bbcomments/";
+	private static final String COMMENT_URL_PREFIX = "http://171.67.77.236/bbcomments/";
+
+	private SyndFeed feed;
+	private List<SyndEntry> rss_entries = new ArrayList<SyndEntry>();
 
 	public CommentCollector(BoingBoingEntry bbentry, int index) {
 		this.bbentry = bbentry;
 		this.index = index;
+
+		feed = new SyndFeedImpl();
+		feed.setFeedType("RSS 2.0");
+
+		feed.setTitle("BoingBoing GIGAprints Comments");
+		feed.setLink("http://hci.stanford.edu/gigaprints");
+		feed.setDescription("");
+		
 		updater = new Thread() {
-			
+
 			long lastRecorded = 0L;
-			
+
 			public void run() {
 				while (true) {
 					try {
@@ -47,12 +75,40 @@ public class CommentCollector extends InkCollector {
 		Ink comment_ink = getInk();
 		// clear();
 
-		File comment_image = new File("data/Blog/comments/comment" + index + "_" + count + ".jpg"); 
-		
+		String comment_filename = "comment" + index + "_" + count + ".jpg";
+		File comment_image = new File(CommentCollector.COMMENT_DIR + comment_filename); 
+
 		InkRenderer r = new InkRenderer(comment_ink);
 		r.renderToJPEG(comment_image, new Pixels(1), new Inches(8.5), new Inches(8.5));
 
-		// TODO: Update RSS feed here
+		SyndEntry entry;
+		SyndContent description;
+
+			try {
+				entry = new SyndEntryImpl();
+				entry.setTitle("Comment " + (count + 1) + " on story \"" + bbentry.title + "\"");
+				entry.setLink(CommentCollector.COMMENT_URL_PREFIX + comment_filename);
+				entry.setPublishedDate(new Date());
+				description = new SyndContentImpl();
+				description.setType("text/plain");
+				description.setValue("<p><img src=\"" + CommentCollector.COMMENT_URL_PREFIX + comment_filename + "\"></p>");
+				entry.setDescription(description);
+				rss_entries.add(entry);
+
+				feed.setEntries(rss_entries);
+				
+				Writer writer = new FileWriter(CommentCollector.COMMENT_DIR + "comments.rss");
+				SyndFeedOutput output = new SyndFeedOutput();
+				output.output(feed,writer);
+				writer.close();
+			} catch (IOException e) {
+				DebugUtils.println("IO exception encountered trying to write RSS file");
+				e.printStackTrace();
+			} catch (FeedException e) {
+				DebugUtils.println("FeedException encountered trying to write RSS file");
+				e.printStackTrace();
+			}
+
 		DebugUtils.println("Stored comment for story \"" + bbentry.title + "\"");
 		count++;
 
