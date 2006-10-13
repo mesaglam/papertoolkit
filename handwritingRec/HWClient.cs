4d481b408b1ba1c33788310fa9557fb426c03866
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-10-13 18:18:08 +0000

    Refactored main cs file. Adding Socket support.

---
+++ handwritingRec/HWClient.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HandwritingRecognition {
+    class HWClient {
+    }
+}
---
+++ handwritingRec/HandwritingRecService.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace HandwritingRecognition {
+    static class HandwritingRecService {
+
+        private static HWServer server;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new HWRecForm());
+
+
+            server = new HWServer();
+            server.getIPAddress();
+        }
+
+
+    }
+}
