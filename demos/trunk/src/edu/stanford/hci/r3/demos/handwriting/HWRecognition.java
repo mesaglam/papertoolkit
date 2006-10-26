7d3440c9a1c84821e6247925f1d6e5ae47624ed7
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-10-26 21:13:06 +0000

    A Demo for Handwriting

---
+++ demos/trunk/src/edu/stanford/hci/r3/demos/handwriting/HWRecognition.java
@@ -0,0 +1,22 @@
+package edu.stanford.hci.r3.demos.handwriting;
+
+/**
+ * <p>
+ * An application that demonstrates the Handwriting Recognition facilities of R3. It also happens to
+ * demonstrate some of the cooler features of R3, such as automatically creating and registering
+ * regions AT RUNTIME.
+ * </p>
+ * <p>
+ * <span class="BSDLicense"> This software is distributed under the <a
+ * href="http://hci.stanford.edu/research/copyright.txt">BSD License</a>. </span>
+ * </p>
+ * 
+ * @author <a href="http://graphics.stanford.edu/~ronyeh">Ron B Yeh</a> (ronyeh(AT)cs.stanford.edu)
+ */
+public class HWRecognition {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
