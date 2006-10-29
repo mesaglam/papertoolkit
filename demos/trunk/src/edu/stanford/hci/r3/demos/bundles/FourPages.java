4da306dfee27079e00cbf9574ccb677ad0d36c4f
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2006-10-29 12:41:36 +0000

    Render 4 Sheets w/ different pattern...

--- demos/trunk/src/edu/stanford/hci/r3/demos/bundles/FourPages.java
+++ demos/trunk/src/edu/stanford/hci/r3/demos/bundles/FourPages.java
@@ -1,11 +1,13 @@
 package edu.stanford.hci.r3.demos.bundles;
 
+import java.awt.Font;
 import java.io.File;
 import java.util.List;
 
 import edu.stanford.hci.r3.paper.Bundle;
 import edu.stanford.hci.r3.paper.Region;
 import edu.stanford.hci.r3.paper.Sheet;
+import edu.stanford.hci.r3.paper.regions.TextRegion;
 import edu.stanford.hci.r3.render.BundleRenderer;
 import edu.stanford.hci.r3.units.Inches;
 
@@ -22,7 +24,18 @@ import edu.stanford.hci.r3.units.Inches;
  */
 public class FourPages {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		renderFourPDFs();
+	}
+
+	/**
+	 * Generate a Bundle and render it to four individual PDFs.
+	 */
+	@SuppressWarnings("unused")
+	private static void renderFourPDFs() {
 		Bundle bundle = new Bundle("Four Pages");
 		bundle.createAndAddSheets(4, "Lined Notes", new Inches(8.5), new Inches(11));
 		List<Sheet> sheets = bundle.getSheets();
@@ -34,7 +47,25 @@ public class FourPages {
 
 		BundleRenderer renderer = new BundleRenderer(bundle);
 		renderer.renderToIndividualPDFs(new File("data/Bundles"), "FourPages");
-		
-		
+	}
+
+	private static void renderWithGlobalRegion() {
+		Bundle bundle = new Bundle("Four Pages");
+		bundle.createAndAddSheets(4, "Lined Notes", new Inches(8.5), new Inches(11));
+
+		TextRegion submitRegion = new TextRegion("Submit", "Submit Form", new Font("Trebuchet MS",
+				Font.PLAIN, 23), new Inches(6), new Inches(9));
+		submitRegion.setActive(true);
+		bundle.addGlobalRegion(submitRegion);
+
+		List<Sheet> sheets = bundle.getSheets();
+		for (Sheet sheet : sheets) {
+			Region r = new Region("Square_" + sheet.getName(), 2, 2, 4, 5);
+			r.setActive(true); // get some pattern on there!
+			sheet.addRegion(r);
+		}
+
+		BundleRenderer renderer = new BundleRenderer(bundle);
+		renderer.renderToIndividualPDFs(new File("data/Bundles"), "FourPages");
 	}
 }
