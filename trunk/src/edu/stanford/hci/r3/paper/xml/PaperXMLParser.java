bf1933f999526de3cd5b7dd9109476ad6da534a8
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2007-03-10 08:01:05 +0000

    XML sample.

--- trunk/src/edu/stanford/hci/r3/paper/xml/PaperXMLParser.java
+++ trunk/src/edu/stanford/hci/r3/paper/xml/PaperXMLParser.java
@@ -8,6 +8,18 @@ package edu.stanford.hci.r3.paper.xml;
  * <span class="BSDLicense"> This software is distributed under the <a
  * href="http://hci.stanford.edu/research/copyright.txt">BSD License</a>. </span>
  * </p>
+ * <p>
+ * Here is some sample Paper XML:
+ * </p>
+ * <sheet width="8.5" height="11">
+ *   <region name="submit" type="button" x="6.5" y="9" w="1.5" h="1">
+ *     <eventHandler type="click" name="onSubmit"/>
+ *   </region>
+ *   <region name="logo" type="image" src="files/logo.png"/>
+ *   <region name="inkCapture" type="capture">
+ *     <eventHandler type="inkWell" name="onInkArrived"/>
+ *   </region>
+ * </sheet>
  * 
  * @author <a href="http://graphics.stanford.edu/~ronyeh">Ron B Yeh</a> (ronyeh(AT)cs.stanford.edu)
  * 
