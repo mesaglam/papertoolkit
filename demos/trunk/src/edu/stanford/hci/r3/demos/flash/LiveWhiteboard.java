17eddba500afaf20e52aa8fc6545909ef1bc72d1
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2007-04-12 17:16:44 +0000

    Whiteboard is a little more robust (works for sci notation).

--- demos/trunk/src/edu/stanford/hci/r3/demos/flash/LiveWhiteboard.java
+++ demos/trunk/src/edu/stanford/hci/r3/demos/flash/LiveWhiteboard.java
@@ -1,15 +1,12 @@
 package edu.stanford.hci.r3.demos.flash;
 
-import edu.stanford.hci.r3.flash.whiteboard.FlashInkRelayServer;
 import edu.stanford.hci.r3.flash.whiteboard.FlashWhiteboard;
 import edu.stanford.hci.r3.pen.Pen;
-import edu.stanford.hci.r3.pen.PenSample;
-import edu.stanford.hci.r3.pen.streaming.listeners.PenListener;
 
 /**
  * <p>
- * Shows how to use the Flash components to assemble a Live Whiteboard that will get data from the
- * local pen device in real time.
+ * Shows how to use the Flash components to assemble a Live Whiteboard that will get data from the local pen
+ * device in real time.
  * </p>
  * <p>
  * <span class="BSDLicense"> This software is distributed under the <a
@@ -19,45 +16,26 @@ import edu.stanford.hci.r3.pen.streaming.listeners.PenListener;
  * @author <a href="http://graphics.stanford.edu/~ronyeh">Ron B Yeh</a> (ronyeh(AT)cs.stanford.edu)
  * 
  */
-public class LiveWhiteboard implements PenListener {
+public class LiveWhiteboard {
 
 	public static void main(String[] args) {
 		new LiveWhiteboard();
 	}
 
-	private FlashInkRelayServer flash;
+	private FlashWhiteboard flashWhiteboard;
 
 	public LiveWhiteboard() {
 		// in R3, there are two ways to make real-time pen and paper applications... One is to use
 		// the Application framework. A second one, which we will use here, is to just attach a pen
 		// listener to a live (local) Pen.
-		Pen pen = new Pen();
-		pen.startLiveMode();
-
-		// just to see the pen coordinates
-		pen.addLivePenListener(this);
-
-		// start the server for sending ink over to Flash
-		flash = new FlashInkRelayServer();
+		
+		// Pen pen = new Pen(); // local pen
+		Pen pen = new Pen("Home", "solaria.stanford.edu"); // remote pen
 
 		// load the Flash component that listens for real-time ink!
 		// basically, just open the HTML page that contains the flash component! =)
-
-		new FlashWhiteboard();
+		flashWhiteboard = new FlashWhiteboard();
+		flashWhiteboard.addPen(pen);
 	}
 
-	@Override
-	public void penDown(PenSample sample) {
-		flash.sendMessage(sample.toXMLString());
-	}
-
-	@Override
-	public void penUp(PenSample sample) {
-		flash.sendMessage(sample.toXMLString());
-	}
-
-	@Override
-	public void sample(PenSample sample) {
-		flash.sendMessage(sample.toXMLString());
-	}
 }
--- demos/trunk/src/edu/stanford/hci/r3/demos/flash/WizardOfOz.java
+++ demos/trunk/src/edu/stanford/hci/r3/demos/flash/WizardOfOz.java
@@ -1,11 +1,20 @@
 package edu.stanford.hci.r3.demos.flash;
 
+import edu.stanford.hci.r3.Application;
 import edu.stanford.hci.r3.PaperToolkit;
+import edu.stanford.hci.r3.flash.whiteboard.FlashWhiteboard;
+import edu.stanford.hci.r3.pen.Pen;
 
 /**
  * <p>
- * Examples of how you might use R3 to create a Wizard of Oz interface to capture pen input, and provide
+ * An example of how you might use R3 to create a Wizard of Oz interface to capture pen input, and provide
  * feedback to an external device that looks like an advanced AI has "recognized" the text and drawings.
+ * 
+ * This doesn't really need to use the R3 App approach, but rather just attaches a Pen Listener to a pen
+ * device, and sends that data over to the Flash Whiteboard component that we have. Later on, when we start
+ * adding buttons, we may need to use the R3 app approach.
+ * 
+ * We'll try doing this with multiple pens.
  * </p>
  * <p>
  * <span class="BSDLicense"> This software is distributed under the <a
@@ -17,10 +26,22 @@ import edu.stanford.hci.r3.PaperToolkit;
 public class WizardOfOz {
 
 	private PaperToolkit paperToolkit;
+	private Pen pen;
+	private Application app;
+	private FlashWhiteboard whiteboard;
 
 	public WizardOfOz() {
 		paperToolkit = new PaperToolkit();
 		// DebugUtils.println(paperToolkit.getProperty("unusedProperty"));
+
+		pen = new Pen("Home", "solaria.stanford.edu");
+
+		whiteboard = new FlashWhiteboard();
+		whiteboard.addPen(pen);
+
+		app = new Application("Wizard of Oz Monitoring");
+		app.addPenInput(pen);
+		paperToolkit.startApplication(app);
 	}
 
 	public static void main(String[] args) {
