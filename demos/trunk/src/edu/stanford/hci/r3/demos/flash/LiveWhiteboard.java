43f4c9aec0fcd137b7e4f321f99b7467533e6f61
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2007-04-15 01:24:53 +0000

    Flash Demo.

--- demos/trunk/src/edu/stanford/hci/r3/demos/flash/LiveWhiteboard.java
+++ demos/trunk/src/edu/stanford/hci/r3/demos/flash/LiveWhiteboard.java
@@ -8,7 +8,7 @@ import edu.stanford.hci.r3.pen.Pen;
 /**
  * <p>
  * Shows how to use the Flash components to assemble a Live Whiteboard that will get data from the local pen
- * device in real time.
+ * device in real time. We can do multiple Whiteboards at the same time!
  * </p>
  * <p>
  * <span class="BSDLicense"> This software is distributed under the <a
@@ -33,12 +33,21 @@ public class LiveWhiteboard {
 		// the Application framework. A second one, which we will use here, is to just attach a pen
 		// listener to a live (local) Pen.
 
+		// Connect to two pens, one local, and one accessible through the network
 		// Pen pen1 = new Pen(); // local pen
-		Pen pen1 = new Pen("Home", "solaria.stanford.edu"); // remote pen
-		// Pen pen2 = new Pen("Laptop Pen", "171.66.51.122");
+		// Pen pen2 = new Pen("Laptop", "171.66.51.122");
+
+		// Connect to two remote pens, both accessible through the network (and hosted 
+		// on computers behind a NAT box)
+		Pen pen1 = new Pen("Pen 1", "solaria.stanford.edu"); // remote pen
+		pen1.setPenServerPort(11100); // forwarded through the nat to machine 1
+
+		Pen pen2 = new Pen("Pen 2", "solaria.stanford.edu"); // remote pen
+		pen2.setPenServerPort(11105); // forwarded through the nat to machine 2
 
 		// load the Flash component that listens for real-time ink!
 		// basically, just open the HTML page that contains the flash component! =)
+		// we pick different ports, so the data can be streamed separately
 		flashWhiteboard1 = new FlashWhiteboard(8989);
 		flashWhiteboard1.addPen(pen1);
 		flashWhiteboard1.setSwatchColor(Color.RED);
@@ -52,13 +61,13 @@ public class LiveWhiteboard {
 		flashWhiteboard2.load();
 
 		flashWhiteboard3 = new FlashWhiteboard(8991);
-		flashWhiteboard3.addPen(pen1);
+		flashWhiteboard3.addPen(pen2);
 		flashWhiteboard3.setSwatchColor(Color.GREEN);
 		flashWhiteboard3.setTitle("Green Pen");
 		flashWhiteboard3.load();
 
 		flashWhiteboard4 = new FlashWhiteboard(8992);
-		flashWhiteboard4.addPen(pen1);
+		flashWhiteboard4.addPen(pen2);
 		flashWhiteboard4.setSwatchColor(Color.ORANGE);
 		flashWhiteboard4.setTitle("Orange Pen");
 		flashWhiteboard4.load();
