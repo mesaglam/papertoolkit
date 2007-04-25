f4555e458f5ae14ba9b5ea703a9f54fa040f2171
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2007-04-25 20:31:28 +0000

    Single Pen Demo.

--- demos/trunk/src/edu/stanford/hci/r3/demos/flash/LiveWhiteboardOnePen.java
+++ demos/trunk/src/edu/stanford/hci/r3/demos/flash/LiveWhiteboardOnePen.java
@@ -1,14 +1,17 @@
 package edu.stanford.hci.r3.demos.flash;
 
 import java.awt.Color;
+import java.util.List;
 
 import edu.stanford.hci.r3.flash.whiteboard.FlashWhiteboard;
 import edu.stanford.hci.r3.pen.Pen;
+import edu.stanford.hci.r3.util.DebugUtils;
+import edu.stanford.hci.r3.util.graphics.ColorUtils;
 
 /**
  * <p>
- * Shows how to use the Flash components to assemble a Live Whiteboard that will get data from the
- * local pen device in real time.
+ * Shows how to use the Flash components to assemble a Live Whiteboard that will get data from the local pen
+ * device in real time.
  * </p>
  * <p>
  * <span class="BSDLicense"> This software is distributed under the <a
@@ -20,24 +23,38 @@ import edu.stanford.hci.r3.pen.Pen;
 public class LiveWhiteboardOnePen {
 
 	public static void main(String[] args) {
+		// Open Four Whiteboards attached to One Pen
+		// new LiveWhiteboardOnePen(4);
 		new LiveWhiteboardOnePen();
 	}
 
-	private FlashWhiteboard flashWhiteboard1;
-
 	public LiveWhiteboardOnePen() {
+		this(2);
+	}
+
+	public LiveWhiteboardOnePen(int numWhiteboards) {
+		if (numWhiteboards < 1) {
+			numWhiteboards = 1;
+		}
+
 		// in R3, there are two ways to make real-time pen and paper applications... One is to use
 		// the Application framework. A second one, which we will use here, is to just attach a pen
 		// listener to a live (local) Pen.
-		Pen pen1 = new Pen(); // local pen
+		Pen pen = new Pen(); // local pen
 
-		// load the Flash component that listens for real-time ink!
-		// basically, just open the HTML page that contains the flash component! =)
-		flashWhiteboard1 = new FlashWhiteboard(8989);
-		flashWhiteboard1.addPen(pen1);
-		flashWhiteboard1.setSwatchColor(new Color(120, 220, 120));
-		flashWhiteboard1.setTitle("Admit Weekend Whiteboard");
-		flashWhiteboard1.load();
+		List<Color> uniqueColors = ColorUtils.getUniqueColors(numWhiteboards);
+		DebugUtils.println(uniqueColors);
+
+		for (int i = 0; i < numWhiteboards; i++) {
+			// load the Flash component that listens for real-time ink!
+			// basically, just open the HTML page that contains the flash component! =)
+			FlashWhiteboard flashWhiteboard;
+			flashWhiteboard = new FlashWhiteboard(8989 + i);
+			flashWhiteboard.addPen(pen);
+			flashWhiteboard.setSwatchColor(uniqueColors.get(i));
+			flashWhiteboard.setTitle("Whiteboard" + i);
+			flashWhiteboard.load();
+		}
 	}
 
 }
