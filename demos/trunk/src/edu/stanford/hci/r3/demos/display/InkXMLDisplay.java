1733aefcc4a588c0c3974368b8721701a7dd6e6e
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2007-05-17 00:29:56 +0000

    A quick and dirty way to load an ink XML file and show it in an InkPanel.

---
+++ demos/trunk/src/edu/stanford/hci/r3/demos/display/InkXMLDisplay.java
@@ -0,0 +1,59 @@
+package edu.stanford.hci.r3.demos.display;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+import edu.stanford.hci.r3.components.InkPanel;
+import edu.stanford.hci.r3.pen.ink.Ink;
+import edu.stanford.hci.r3.util.DebugUtils;
+import edu.stanford.hci.r3.util.WindowUtils;
+
+/**
+ * <p>
+ * </p>
+ * <p>
+ * <span class="BSDLicense"> This software is distributed under the <a
+ * href="http://hci.stanford.edu/research/copyright.txt">BSD License</a>. </span>
+ * </p>
+ * 
+ * @author <a href="http://graphics.stanford.edu/~ronyeh">Ron B Yeh</a> (ronyeh(AT)cs.stanford.edu)
+ */
+public class InkXMLDisplay {
+
+	/**
+	 * 
+	 */
+	private static InkPanel inkPanel;
+
+	/**
+	 * @return
+	 */
+	private static InkPanel getInkPanel() {
+		if (inkPanel == null) {
+			inkPanel = new InkPanel();
+		}
+		return inkPanel;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setContentPane(getInkPanel());
+		f.setSize(1024, 768);
+		f.setLocation(WindowUtils.getWindowOrigin(f, WindowUtils.DESKTOP_CENTER));
+		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		f.setVisible(true);
+
+		final JFileChooser fc = new JFileChooser();
+		int returnVal = fc.showOpenDialog(f);
+
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			Ink i = new Ink(fc.getSelectedFile());
+			DebugUtils.println(i.getNumStrokes() + " strokes loaded.");
+			getInkPanel().addInk(i);
+		}
+	}
+}
