f7a595e1edd1608fca0d4de6d45692c4c2d47661
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2007-05-21 02:00:47 +0000

    Added explicit support for resolution (PixelsPerInch.java) Fixed a Bug in the PS template. You need a newline to make the Postscript Render correctly!

---
+++ trunk/src/edu/stanford/hci/r3/units/conversion/PixelsPerInch.java
@@ -0,0 +1,48 @@
+package edu.stanford.hci.r3.units.conversion;
+
+/**
+ * <p>
+ * Basically, it wraps a Double, and is used to interpret the size of images.
+ * </p>
+ * <p>
+ * <span class="BSDLicense"> This software is distributed under the <a
+ * href="http://hci.stanford.edu/research/copyright.txt">BSD License</a>. </span>
+ * </p>
+ * 
+ * @author <a href="http://graphics.stanford.edu/~ronyeh">Ron B Yeh</a> (ronyeh(AT)cs.stanford.edu)
+ */
+public class PixelsPerInch {
+
+	/**
+	 * Defaults to 72, because web browsers and Java2D consider images 72 dpi.
+	 */
+	private double value = 72;
+
+	/**
+	 * 
+	 */
+	public PixelsPerInch() {
+		// 72 ppi
+	}
+
+	/**
+	 * @param ppi
+	 */
+	public PixelsPerInch(double ppi) {
+		setValue(ppi);
+	}
+
+	/**
+	 * @return
+	 */
+	public double getValue() {
+		return value;
+	}
+
+	/**
+	 * @param ppi
+	 */
+	public void setValue(double ppi) {
+		value = ppi;
+	}
+}
