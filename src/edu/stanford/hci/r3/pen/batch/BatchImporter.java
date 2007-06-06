cfb2fab5de9a514ce1015b484cbe3bc54d40ecbb
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2007-06-06 02:38:05 +0000

    Figuring out a way to register monitors... sort of like Microsoft's COM... =}

--- src/edu/stanford/hci/r3/pen/batch/BatchImporter.java
+++ src/edu/stanford/hci/r3/pen/batch/BatchImporter.java
@@ -9,15 +9,22 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import javax.swing.JOptionPane;
+
+import edu.stanford.hci.r3.demos.batched.monitor.BatchedMonitor;
+import edu.stanford.hci.r3.util.DebugUtils;
 import edu.stanford.hci.r3.util.SystemUtils;
 
 /**
  * <p>
- * The batch importer will be called by the pen importer (.NET code) every time you synchronize the
- * pen. It sends the information to the BatchServer, which will notify any running applications.
+ * The batched importer will be called by the pen importer (.NET code) every time you synchronize
+ * the pen. It sends the information to the BatchServer, which will notify any running applications.
  * 
  * TODO: In the future, applications do not need to be running all the time. They will be notified
  * of new data upon booting.
+ * 
+ * This class is launched by PaperToolkit\penSynch\bin\BatchImporter.exe. It creates a log file in
+ * the same directory, that you can tail using an app such as BareTail.
  * </p>
  * <p>
  * <span class="BSDLicense"> This software is distributed under the <a
@@ -27,22 +34,35 @@ import edu.stanford.hci.r3.util.SystemUtils;
  * @author <a href="http://graphics.stanford.edu/~ronyeh">Ron B Yeh</a> (ronyeh(AT)cs.stanford.edu)
  */
 public class BatchImporter {
-	public static void main(String[] args) {
+
+	private static final boolean DEBUG = true;
+
+	public BatchImporter(String[] args) {
 		PrintWriter pw = null;
 		try {
+			if (DEBUG) {
+				JOptionPane.showMessageDialog(null, "Batched Pen Data saved to penSynch/Data/XML/."
+						+ "Notifying Registered Listeners...");
+			}
+
 			pw = new PrintWriter(new File("BatchImporter.log"));
 			pw.println("Running the Batched Pen Data Importer");
 			for (String arg : args) {
 				pw.println("Argument: " + arg);
 			}
 
+			// find all registered listeners, and run them in sequence, with the correct arguments
+
 			// open a socket connection to the batch importer / event handler
+			// if an app is running, it will handle the incoming batched ink...
+			// if not, this will simply throw an exception and fall through...
 			final Socket socket = new Socket("localhost", BatchServer.DEFAULT_PLAINTEXT_PORT);
 			final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket
 					.getOutputStream()));
 
 			// send over the xml file! =)
-			bw.write("XML: " + args[0] + SystemUtils.LINE_SEPARATOR); // the path!
+			bw.write("XML: " + args[0] + SystemUtils.LINE_SEPARATOR); // the
+			// path!
 			bw.write(BatchServer.EXIT_COMMAND + SystemUtils.LINE_SEPARATOR);
 			bw.flush();
 			bw.close();
@@ -63,5 +83,33 @@ public class BatchImporter {
 			pw.flush();
 			pw.close();
 		}
+
+	}
+
+	/**
+	 * Adds a monitor into the configuration file. This monitor
+	 */
+	public static void registerMonitor(BatchImportMonitor monitor) {
+		DebugUtils.println("Registering " + monitor.getName());
+
+		// figure out its path / classpath...
+
+		// save it to our configuration file
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new BatchImporter(args);
+	}
+
+	public static void listRegisteredMonitors() {
+		// TODO Auto-generated method stub
+	}
+
+	public static void removeMonitor(BatchImportMonitor monitor) {
+		// TODO Auto-generated method stub
 	}
 }
