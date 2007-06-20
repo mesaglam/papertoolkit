c631c8982ad5f4b3159ee46138ec56d00df00a39
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2007-06-20 08:22:43 +0000

    For my video prototyping...

---
+++ demos/trunk/src/edu/stanford/hci/r3/demos/unfinished/MapApp.java
@@ -0,0 +1,20 @@
+package edu.stanford.hci.r3.demos.unfinished;
+
+import edu.stanford.hci.r3.PaperToolkit;
+import edu.stanford.hci.r3.application.Application;
+
+public class MapApp extends Application {
+
+	public MapApp() {
+		super("Maps!");
+		// TODO Auto-generated constructor stub
+
+		
+		
+	}
+
+	public static void main(String[] args) {
+		MapApp maps = new MapApp();
+		PaperToolkit.runApplication(maps);
+	}
+}
