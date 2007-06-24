aebacee07b658448c7ca12714391ee297632017b
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2007-06-24 20:18:30 +0000

    Fake Demo for Video Prototype...

--- demos/trunk/src/edu/stanford/hci/r3/demos/unfinished/MapApp.java
+++ demos/trunk/src/edu/stanford/hci/r3/demos/unfinished/MapApp.java
@@ -2,19 +2,53 @@ package edu.stanford.hci.r3.demos.unfinished;
 
 import edu.stanford.hci.r3.PaperToolkit;
 import edu.stanford.hci.r3.application.Application;
+import edu.stanford.hci.r3.events.PenEvent;
+import edu.stanford.hci.r3.events.handlers.ClickHandler;
+import edu.stanford.hci.r3.paper.Region;
+import edu.stanford.hci.r3.paper.Sheet;
 
 public class MapApp extends Application {
 
+	private Sheet s;
+
 	public MapApp() {
 		super("Maps!");
-		// TODO Auto-generated constructor stub
 
+		
+		Region r = new Region("Click", 2, 3, 4, 4);
+		r.addEventHandler(new ClickHandler() {
+			@Override
+			public void clicked(PenEvent e) {
+				device.display(....)
+				
+			}
+
+			@Override
+			public void pressed(PenEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void released(PenEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+
+		
+		
 		
 		
 	}
 
 	public static void main(String[] args) {
-		MapApp maps = new MapApp();
-		PaperToolkit.runApplication(maps);
+		
+		Application map = new MapApp();
+		
+		PaperToolkit.runApplication(map);
+		
+		
+		
 	}
 }
