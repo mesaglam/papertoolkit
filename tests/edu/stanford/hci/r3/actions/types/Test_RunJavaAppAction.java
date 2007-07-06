ebc5c1e24fe4927aa3b03b6f0e73250707cca3d9
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2007-07-06 00:35:36 +0000

    Removing the "unit tests"... I will try to instead have better documentation and actual example code in the demos directory that utilizes & tests each class. Goodbye!

--- tests/edu/stanford/hci/r3/actions/types/Test_RunJavaAppAction.java
+++
@@ -1,8 +0,0 @@
-package edu.stanford.hci.r3.actions.types;
-
-public class Test_RunJavaAppAction {
-	public static void main(String[] args) {
-		RunJavaAppAction action = new RunJavaAppAction(DummyWithMain.class);
-		action.invoke();
-	}
-}
