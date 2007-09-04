d2aec5e62f9c2c72bbde0b3074bb78f1d9d98df0
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2007-09-04 21:50:20 +0000

    Sweet... firefox works again. Now to make it uber awesome.

---
+++ firefox/sidecarviz/chrome/content/sidecarvizOverlay.js
@@ -0,0 +1,12 @@
+// This is our javascript
+
+
+function copyToSideCar() {
+	alert("Copy to SideCar XXX");
+}
+
+function startSideCar() {
+	// question, can we access the HTML's DOM?
+	// if we can, we can potentially talk to Flex!!! Damn... =)
+	alert("Starting SideCar XXX");
+}
