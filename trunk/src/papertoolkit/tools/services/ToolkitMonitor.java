f8f08918fe9bc6d1e03063cb44aee931e4cfb54a
ronyeh <ronyeh@08f22fb0-ad19-0410-ab50-7f24de69d321>
2007-09-13 01:40:16 +0000

    Sending events over to Sidecar now!

--- trunk/src/papertoolkit/tools/services/ToolkitMonitor.java
+++ trunk/src/papertoolkit/tools/services/ToolkitMonitor.java
@@ -1,7 +1,10 @@
 package papertoolkit.tools.services;
 
+import java.util.List;
+
 import papertoolkit.events.EventHandler;
 import papertoolkit.events.PenEvent;
+import papertoolkit.paper.Region;
 import papertoolkit.pen.InputDevice;
 import papertoolkit.pen.PenSample;
 
@@ -25,11 +28,37 @@ public class ToolkitMonitor {
 		monitoringService = toolkitMonitoringService;
 	}
 
+	private EventHandler lastEventHandler;
+	private long lastEventHandlerTimestamp = 0L;
+
+	/**
+	 * Report when event handlers change...
+	 * 
+	 * @param handler
+	 * @param event
+	 */
 	public void eventHandled(EventHandler handler, PenEvent event) {
+		// intercept system outs too!
+		// using System.setOut and the DebugUtils trick
+
 		if (handler != null) {
-			monitoringService.outputToClients("Event Handled: " + handler + " :: " + event);
+			// show a new group entry if it has changed...
+			// otherwise, send all pen downs and ups to be displayed in the datagrid
+			if (lastEventHandler != handler || (event.getTimestamp() - lastEventHandlerTimestamp) > 3000) {
+
+				// assume handlers are anonymous subclasses
+				// get the super class's name
+				List<Region> parentRegions = handler.getParentRegions();
+
+				monitoringService.outputToClients("<eventHandler component=\"" + parentRegions.toString()
+						+ "\" handlerName=\"" + handler.getClass().getSuperclass().getSimpleName()
+						+ "\" time=\"" + event.getTimestamp() + "\"/>");
+				lastEventHandler = handler;
+				lastEventHandlerTimestamp = event.getTimestamp();
+			}
 		} else {
-			monitoringService.outputToClients("Unhandled Event: " + event);
+			// TODO: Dump these into the pen data panel somewhere
+			// monitoringService.outputToClients("Unhandled Pen Strokes: " + event);
 		}
 	}
 
